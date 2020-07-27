6789f5ea31a821cd7ca03fea080a28c06b7d89a2
Umang G <ujg2794@rit.edu>
2020-07-27 17:55:55 -0400

    String convert problem solution with unit tests

---
+++ src/test/java/com/directu/problemsolving/dynamic/StringConvertTest.java
@@ -0,0 +1,30 @@
+package com.directu.problemsolving.dynamic;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * StringConvertTest
+ *
+ * @author Umang G
+ */
+class StringConvertTest {
+  private static Stream<Arguments> testCases() {
+    return Stream.of(
+        Arguments.of("beard", "bored", 9),
+        Arguments.of("ababababababa", "abcabcabcabc", 21),
+        Arguments.of("winter", "spring", 23)
+    );
+  }
+
+  @ParameterizedTest(name = "{index} > {0}, {1} > ")
+  @MethodSource("testCases")
+  public void testMaximumSumWithNoThreeConsecutiveElement(String toConvert, String toMatch, int cost) {
+    assertEquals(cost, StringConvert.convertString(toConvert, toMatch));
+  }
+}
